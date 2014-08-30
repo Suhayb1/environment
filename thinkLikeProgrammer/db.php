e68690d1b29128ebcc44fb63d462d1dc77c59f2b
Suhayb1 <suhayb.dweikat@gmail.com>
2014-08-30 12:08:51 +0300

    Revert "addanddeletesomefiles"
    This reverts commit 9d4d134316bdb9f59e1b4b20c94f66c0cf673bc4.

--- thinkLikeProgrammer/db.php
+++
@@ -1,4 +0,0 @@
-<?php
-$link=mysql_connect("localhost","root","root") or die("not connect");
-mysql_select_db("tlprogrammer",$link);
-?>
--- thinkLikeProgrammer/index.php
+++ thinkLikeProgrammer/index.php
@@ -1,61 +1,40 @@
 <?php
-session_start();
-$page=$_GET["page"];
-if($page=="games"||$page=="levels"||$page=="questions"){///////////////////////here put ||"pagename require sign in"
+    include('bootstrap/bootstrap.php');
+    session_start();
+    include("log in/connect.php");
+    $_SESSION['username']="??";
+    $_SESSION['admin']="??";
+    session_destroy();
 
-if(!isset($_SESSION['account']))
-{
-session_destroy();
-echo '<meta http-equiv="refresh" content="0;url=home.php?page=login">';
-
-}
-}
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
     <head>
         <meta charset="utf-8" />
-        <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
-        <link href="bootstrap/css/custom.css" rel="stylesheet">
-        
         <title></title>
     </head>
-    <body class="bodycolor">
-        <div class="container headcolor">
-	        <div class="container-fluid">
-	         	<header class="h1 headerfont ">Think Like Programmer</header>
-	        </div>
-	    </div>
-	    <div class="row container">   
-		 	<div class="col-lg-2 ">    
-				<div class="list-group ">
-					  <a href="#" class="list-group-item disabled">
-					   pages
-					  </a>
-					  <a href="index.php?page=games" class="list-group-item">Games </a>
-					  <a href="index.php?page=test" class="list-group-item">test(Morbi leo risus)</a>
-					  				</div>
-				<div class="row"></div>
-			</div>
-			<div class="col-lg-10 margin-b-10 " style="background-color:white"></div>
-			<div class="col-lg-10">
-				<div class="container "> 
-	        		
-	        			<?php
-							$page=$_GET["page"];
-							$page1=$page.".php";
-							if(is_file($page1))
-								include($page1);
-							else
-								include("games.php");
-      					  ?>
-
-	        		
-        		</div>
-        	</div>
-		</div>	              
-        <script src="js/jquery.js"></script>
-        <script src="js/bootstrap.js"></script>
+    <body>
+        <div>
+            <h1>
+                user login
+            </h1>
+            <form method="post" action="log in/index.php">
+               <table class="table table-striped">
+                   <tr>
+                        <td>user name </td>
+                        <td><input type="text" name="username"></td>
+                   </tr>
+                   <tr>
+                       <td>password</td>
+                       <td><input type="text" name="pass"></td> 
+                   </tr>
+                  <tr><td>
+                    <input type="submit" value="log in">
+                </tr>
+               </table>
+            
+            </form>
+        </div>
     </body>
 </html>
